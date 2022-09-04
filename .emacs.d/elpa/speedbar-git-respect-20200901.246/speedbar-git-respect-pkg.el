f80adc3a9b5186199e6a505c99e2e4e38673c345
Mehul Singh <mehulsingh072001@gmail.com>
2022-09-04 06:23:10 +0530

    adding only important files

--- .emacs.d/elpa/speedbar-git-respect-20200901.246/speedbar-git-respect-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from speedbar-git-respect-20200901.246/speedbar-git-respect.el  -*- no-byte-compile: t -*-
-(define-package "speedbar-git-respect" "20200901.246" "Particular respect git repo in speedbar" '((f "0.8.0") (emacs "25.1")) :commit "dd8f0849fc1dd21b42380e1a8c28a9a29acd9511" :authors '(("Muromi Ukari" . "chendianbuji@gmail.com")) :maintainer '("Muromi Ukari" . "chendianbuji@gmail.com") :url "https://github.com/ukari/speedbar-git-respect")
